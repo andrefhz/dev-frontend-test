7a8ea51cce9faa36f689ff4ce12e29ed1d94d879
André Cavalcanti <andre.cavalcanti.simoes@gmail.com>
2025-09-14 22:18:26 -0300

    favicon nas paginas

--- contato.php
+++ contato.php
@@ -10,6 +10,8 @@
     <meta property="og:description" content="Automação, manutenção preventiva e consultoria para indústrias que buscam eficiência e inovação.">
     <title>Contato - <?=$nomeEmpresa;?></title>
 
+    <link rel="icon" href="image/favicon.png" type="image/png">
+
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/slick.css">
